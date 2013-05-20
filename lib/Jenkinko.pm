f6779161c325b63835b4a431f7af286e2ffba6a7
Naoya Ito <i.naoya@gmail.com>
2013-05-20 15:05:48 +0900

    Change to master

--- lib/Jenkinko.pm
+++ lib/Jenkinko.pm
@@ -4,6 +4,11 @@ use Mouse;
 has 'name' => ( is => 'rw', isa => 'Str');
 has 'age'  => ( is => 'rw', isa => 'Int');
 
+sub hello {
+    my $self = shift;
+    sprintf "My name is %s and %d years old.", $self->name, $self->age;
+}
+
 __PACKAGE__->meta->make_immutable;
 
 __END__
